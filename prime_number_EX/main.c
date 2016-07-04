952b476cada1246c6601eebe0f91c4e3b9ee1cbe
doomout1 <doomout1@gmail.com>
2016-07-04 20:03:17 +0900

    prime_numver_EX

---
+++ prime_number_EX/main.c
@@ -0,0 +1,26 @@
+//1~50 중 소수 구하기
+#include <stdio.h>
+#define PRIME_MAX 50 //소수 최대값
+
+int main()
+{
+    char num[PRIME_MAX + 1];
+    int i , j;
+
+    //배열 초기화
+    for(i=0; i<=PRIME_MAX; i++)
+        num[i] = 0;
+
+    //소수인지 판별
+    for(i=2; i<=PRIME_MAX; i++)
+    {
+        if(num[i] == 0)
+        {
+            printf(" %d", i);
+            for(j=2*i; j<=PRIME_MAX; j+=i)
+                num[j] = 1;
+        }
+    }
+    printf(" \n");
+    return 0;
+}
